20d6579dee14ccde9addc4c4066fdd24fd1dff83
fanghsu <tfsheu@gmail.com>
2013-10-19 15:09:16 +0800

    使用 模組化方式 #10

--- main.js
+++ main.js
@@ -1,17 +1,29 @@
 
-(function () {
-var canvas=document.getElementById('game');
-var ctx=canvas.getContext('2d');
-var ballX = Math.floor(Math.random() * 300); // 0..300
-var ballY = Math.floor(Math.random() * 500);
-var ballR = Math.floor(Math.random() * 100);
+var gameModule = (function (){ 
 
-canvas.width = 480;
-canvas.height = 320;
+	
 
-ctx.fillstyle ='black';
-ctx.beginPath();
-ctx.arc(ballX, ballY, ballR, 0, Math.PI, true)
-ctx.fill();
+	function start() {
+	var canvas=document.getElementById('game');
+	var ctx=canvas.getContext('2d');
+	var ballX = Math.floor(Math.random() * 300); // 0..300
+	var ballY = Math.floor(Math.random() * 500);
+	var ballR = Math.floor(Math.random() * 100);
+
+	canvas.width = 480;
+	canvas.height = 320;
+
+	ctx.fillstyle ='black';
+	ctx.beginPath();
+	ctx.arc(ballX, ballY, ballR, 0, Math.PI, true)
+	ctx.fill();
+
+	}
+
+	return {
+		hello: start
+	}
 }) ();
+
+gameModule.hello();
 
