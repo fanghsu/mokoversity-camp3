81f76d5dd8981e34cdc4e9e3eae02f9a5ee3fc3c
fanghsu <tfsheu@gmail.com>
2013-10-20 16:15:55 +0800

    加註解

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/user', user.list);
 app.get('/scores', user.save);
-//app.get('/user', user.save);
+//app.get('/scores', scores.list); 只能定義一個來承接, 後面那個部會有動作
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
