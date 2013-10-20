740a49b367a9f086bda15a13c81c1916534d7041
fanghsu <tfsheu@gmail.com>
2013-10-20 11:18:37 +0800

    score 計算 fix #12

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 
 var gameModule = (function (){ 
 
-	var timeoutVar, count = 0;
+	var timeoutVar, 
+		count = 0,
+		ballX,
+		ballY,
+		ballR,
+		score=0;
     
 	function touchEvent(evt){
 		var x = evt.clientX, 
-			y = evt.clientY;
+			y = evt.clientY,
+			tmp;
 
-			console.log("Click:" + x + "," + y);
+			tmp = (ballX-x)*(ballX-x) + (ballY-y)*(ballY-y);
+			//console.log("click:" + ballX + "," + x);
+
+			if(tmp<ballR*ballR){
+				score += 100 - ballR;
+				console.log("Hit: +" + (100 - ballR));
+
+			}
+
+			
 
 	}
 
@@ -21,12 +36,15 @@ var gameModule = (function (){
 	function startGame() {
 		var canvas=document.getElementById('game');
 		var ctx=canvas.getContext('2d');
-		var ballX = Math.floor(Math.random() * 500); // 0..300
-		var ballY = Math.floor(Math.random() * 600);
-		var ballR = Math.floor(Math.random() * 100);
+
 		var colorR = Math.floor(Math.random() * 256)%256;
 		var colorG = Math.floor(Math.random() * 256)%256;
 		var colorB = Math.floor(Math.random() * 256)%256;
+
+
+		ballX = Math.floor(Math.random() * 500); // 0..300
+		ballY = Math.floor(Math.random() * 600);
+		ballR = Math.floor(Math.random() * 100);
 
 		canvas.width = 640;
 		canvas.height = 480;
@@ -38,8 +56,8 @@ var gameModule = (function (){
 		ctx.arc(ballX, ballY, ballR, 0, Math.PI*2, true);
 		ctx.fill();
 
-		if (count >=10){
-
+		if (count >=20){
+			gameOver();
 		} else{
 
 			timeoutVar = setTimeout(startGame, 1000);
@@ -50,12 +68,10 @@ var gameModule = (function (){
 	}
 
 	function gameOver(){
-		if (count >=10){
-
-		}else{
-			timeoutVar = setTimeout(gameOver, 1000);
-			console.log("CounterB: " + count);
-		}
+		
+		console.log("Score: " + score);
+		ballR = 0;
+	
 	}
 
 	return{
